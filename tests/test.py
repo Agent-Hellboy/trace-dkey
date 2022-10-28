2966ae4c500cb0b8717b62feb11b0bf005fda361
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-10-28 10:35:48 +0530

    Added new tests

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,9 @@
 from trace_dkey import trace
 
-def test_trace():
-    l={'a':{'b':{'c':{'d':{'e':{'f':1}}}}}}
+def test_trace_1():
+    l = {'a':{'b':{'c':{'d':{'e':{'f':1}}}}}}
     assert l['a']['b']['c']['d']['e']['f']==1
+
+def test_trace_2():
+    l = {'a':{'b':{'c':{'d':{'e':{'f':('h','i')},'g':1}}}}}
+    assert l['a']['b']['c']['d']['e']['f']==('h','i')
