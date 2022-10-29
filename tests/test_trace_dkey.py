c25df8066fad1074b0df4774da34f50ac3158e53
Luiz Paiva <luizgustavodematos@gmail.com>
2022-10-29 14:44:49 -0300

    Fix unit tests to assert using the returned paths

--- tests/test_trace_dkey.py
+++ tests/test_trace_dkey.py
@@ -8,29 +8,28 @@ import pytest
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 from trace_dkey import trace 
 
-def get_stdout_of_trace(l,p):
-    f = io.StringIO()
-    with redirect_stdout(f):
-        trace(l,p)
-    return f.getvalue()
-
 
 @pytest.fixture
 def test_trace_fixture():
     dict1 = {'a': {'b': {'c': {'d': {'e': {'f': 1}}}}}}
-    fixture1 = get_stdout_of_trace(dict1,'f')
+    fixture1 = trace(dict1, 'f')
     dict2 = {'a': {'b': {'c': {'d': {'e': {('h','i'): ('h', 'i')}, 'g': 1}}}}}
-    fixture2 = get_stdout_of_trace(dict2,('h','i'))
+    fixture2 = trace(dict2, ('h','i'))
     dict3 = {'a': {'b':{'c':{('d','e'):1}}}}
-    fixture3 = get_stdout_of_trace(dict2,'r')
-    return fixture1, fixture2, fixture3
+    fixture3 = trace(dict3, 'r')
+    dict4 = {'a': {'b': {'c': {('d','e'): 1}}, 'c': 1}}
+    fixture4 = trace(dict4, 'c')
+    return fixture1, fixture2, fixture3, fixture4
 
 
 def test_when_key_is_string(test_trace_fixture):
-    assert test_trace_fixture[0][:-1] == f"Found 'f' at  a--> b--> c--> d--> e-->f"
+    assert test_trace_fixture[0] == [['a', 'b', 'c', 'd', 'e', 'f']]
 
 def test_when_key_is_tuple(test_trace_fixture):
-    assert test_trace_fixture[1][:-1] == f"Found '('h', 'i')' at  a--> b--> c--> d--> e-->('h', 'i')"
+    assert test_trace_fixture[1] == [['a', 'b', 'c', 'd', 'e', ('h', 'i')]]
 
 def test_when_key_is_not_present(test_trace_fixture):
-    assert test_trace_fixture[2][:-1] == ''
+    assert test_trace_fixture[2] == []
+
+def test_when_multiple_keys_found(test_trace_fixture):
+    assert test_trace_fixture[3] == [['a', 'b', 'c'], ['a', 'c']]
